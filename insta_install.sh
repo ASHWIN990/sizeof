ed73297a7b9c8b9eff8d79383c864da1b64a84de
ASHWIN990 <ashwinisahu990@gmail.com>
2020-05-24 00:33:57 +0530

    Relased

--- insta_install.sh
+++ insta_install.sh
@@ -13,11 +13,11 @@ fi
 
 install(){
     curl https://raw.githubusercontent.com/ASHWIN990/sizeof/master/sizeof --output /tmp/sizeof
-    chmod +x /tmp/sizeof | echo -e "\e[93;1mCHANGING THE FILE PERMISSIONS\e[0m"
-    mv -f /tmp/sizeof /usr/bin/ | echo -e "\e[93;1mMOVING THE sizeof TO /usr/bin\e[0m"
+    chmod +x /tmp/sizeof | echo -e "CHANGING THE FILE PERMISSIONS"
+    mv -f /tmp/sizeof /usr/bin/ | echo -e "MOVING THE sizeof TO /usr/bin"
     curl https://raw.githubusercontent.com/ASHWIN990/sizeof/master/.man/sizeof.1.gz --output /tmp/sizeof.1.gz
     mv -f /tmp/sizeof.1.gz /usr/local/share/man/man1/sizeof.1.gz
-    mandb -q | echo -e "\e[93;1mUPDATING AND INSTALLING THE MANDB\e[0m"
+    mandb -q | echo -e "UPDATING AND INSTALLING THE MANDB"
     }
 
 
